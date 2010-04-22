a7686ba3f51df1390d909f516919d85b49085bda
Chris McDonough <chrism@plope.com>
2010-04-22 12:53:33 +0000

    No use in ZPL if we don't depend on Zope stuff.

--- internatl/__init__.py
+++ internatl/__init__.py
@@ -1,17 +1,3 @@
-##############################################################################
-#
-# Copyright (c) 2004 Zope Corporation and Contributors.
-# All Rights Reserved.
-#
-# This software is subject to the provisions of the Zope Public License,
-# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE.
-#
-##############################################################################
-
 import re
 
 NAME_RE = r"[a-zA-Z][-a-zA-Z0-9_]*"
