2b3c2b5b56439ed542edc50833463866dfbde281
Joe Dallago <jd.dallago@gmail.com>
2011-05-28 00:18:11 -0500

    Started porting the library to Py3.  Replaced all of the instances of unicode classes, objects, and functions with the Six library text_type constant.  Changed all unicode literals to the u() function from the six library.  Changed each instance of basestring with the string_types Six constant.  I went through tests and the main module.

--- translationstring/tests/test__init__.py
+++ translationstring/tests/test__init__.py
@@ -1,4 +1,6 @@
 import unittest
+from six import text_type
+from six import u
 
 class TestTranslationString(unittest.TestCase):
     def _getTargetClass(self):
@@ -9,9 +11,9 @@ class TestTranslationString(unittest.TestCase):
         klass = self._getTargetClass()
         return klass(msgid, **kw)
 
-    def test_is_unicode_subclass(self):
+    def test_is_text_type_subclass(self):
         inst = self._makeOne('msgid')
-        self.failUnless(isinstance(inst, unicode))
+        self.failUnless(isinstance(inst, text_type))
 
     def test_msgid_is_translation_string(self):
         another = self._makeOne('msgid', domain='domain', default='default',
@@ -45,55 +47,55 @@ class TestTranslationString(unittest.TestCase):
         inst = self._makeOne('This is $name version ${version}.',
                               mapping=mapping)
         result = inst.interpolate()
-        self.assertEqual(result, u'This is Zope version 3.')
+        self.assertEqual(result, u('This is Zope version 3.'))
 
     def test_interpolate_subsitution_more_than_once(self):
         mapping = {"name": "Zope", "version": 3}
         inst = self._makeOne(
-            u"This is $name version ${version}. ${name} $version!",
+            u("This is $name version ${version}. ${name} $version!"),
             mapping=mapping)
         result = inst.interpolate()
-        self.assertEqual(result, u'This is Zope version 3. Zope 3!')
+        self.assertEqual(result, u('This is Zope version 3. Zope 3!'))
 
     def test_interpolate_double_dollar_escape(self):
         mapping = {"name": "Zope", "version": 3}
         inst = self._makeOne('$$name', mapping=mapping)
         result = inst.interpolate()
-        self.assertEqual(result, u'$$name')
+        self.assertEqual(result, u('$$name'))
 
     def test_interpolate_missing_not_interpolated(self):
         mapping = {"name": "Zope", "version": 3}
         inst = self._makeOne(
-            u"This is $name $version. $unknown $$name $${version}.",
+            u("This is $name $version. $unknown $$name $${version}."),
             mapping=mapping)
         result = inst.interpolate()
         self.assertEqual(result,
-                         u'This is Zope 3. $unknown $$name $${version}.')
+                         u('This is Zope 3. $unknown $$name $${version}.'))
 
     def test_interpolate_missing_mapping(self):
-        inst = self._makeOne(u"This is ${name}")
+        inst = self._makeOne(u("This is ${name}"))
         result = inst.interpolate()
-        self.assertEqual(result, u'This is ${name}')
+        self.assertEqual(result, u('This is ${name}'))
 
     def test_interpolate_passed_translated(self):
         mapping = {"name": "Zope", "version": 3}
-        inst = self._makeOne(u"This is ${name}", mapping = mapping)
+        inst = self._makeOne(u("This is ${name}"), mapping = mapping)
         result = inst.interpolate('That is ${name}')
-        self.assertEqual(result, u'That is Zope')
+        self.assertEqual(result, u('That is Zope'))
 
     def test___reduce__(self):
         klass = self._getTargetClass()
         inst = self._makeOne('msgid', default='default', domain='domain',
                              mapping='mapping')
         result = inst.__reduce__()
-        self.assertEqual(result, (klass, (u'msgid', 'domain', u'default', 
+        self.assertEqual(result, (klass, (u('msgid'), 'domain', u('default'), 
                                           'mapping')))
 
     def test___getstate__(self):
         inst = self._makeOne('msgid', default='default', domain='domain',
                              mapping='mapping')
         result = inst.__getstate__()
-        self.assertEqual(result, (u'msgid', 'domain', u'default', 'mapping'))
+        self.assertEqual(result, (u('msgid'), 'domain', u('default'), 'mapping'))
 
 class TestTranslationStringFactory(unittest.TestCase):
     def _makeOne(self, domain):
@@ -134,11 +136,11 @@ class TestChameleonTranslate(unittest.TestCase):
         result = translate(msgid)
         self.assertEqual(result, 'interpolated')
 
-    def test_msgid_unicode_translator_is_None(self):
-        msgid = u'foo'
+    def test_msgid_text_type_translator_is_None(self):
+        msgid = u('foo')
         translate = self._makeOne(None)
         result = translate(msgid)
-        self.assertEqual(result, u'foo')
+        self.assertEqual(result, u('foo'))
 
     def test_msgid_translationstring_translator_is_not_None(self):
         msgid = DummyTranslationString()
@@ -148,7 +150,7 @@ class TestChameleonTranslate(unittest.TestCase):
         result = translate(msgid)
         self.assertEqual(result, msgid)
 
-    def test_msgid_unicode_translator_is_not_None(self):
+    def test_msgid_text_type_translator_is_not_None(self):
         msgid = 'foo'
         def translator(msg):
             return msg
@@ -343,10 +345,10 @@ class DummyTranslations(object):
         self.asked_domain = domain
         return self.result
 
-class DummyTranslationString(unicode):
+class DummyTranslationString(text_type):
     def __new__(cls, msgid='', domain=None, default=None, mapping=None):
-        self = unicode.__new__(cls, msgid)
-        unicode.__init__(self, msgid)
+        self = text_type.__new__(cls, msgid)
+        text_type.__init__(self, msgid)
         self.domain = domain
         self.mapping = mapping
         if default is None:
