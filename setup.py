f3fa63ab1e087792fc31397c47151b6d10318b1d
Chris McDonough <chrism@plope.com>
2011-07-21 13:38:52 -0400

    add python 3 trove classifier

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(name='translationstring',
       classifiers=[
           "Intended Audience :: Developers",
           "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
           "Topic :: Software Development :: Internationalization",
           "Topic :: Software Development :: Localization",
           ],
